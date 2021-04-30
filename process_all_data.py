0a7e23cfad975b8909011d0cd5eda5575cc370e3
Lukas <Lukas.Petzold1@mailbox.tu-dresden.de>
2021-04-30 13:32:37 +0200

    removed 0 point jumps and cleaned up the names of the jumps

--- process_all_data.py
+++ process_all_data.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-def read_all_data():
+def read_data(name):
 
-    all_data = pd.read_csv("Sprungdaten_processed/all_data.csv")
+    all_data = pd.read_csv("Sprungdaten_processed/" + name)
 
     return all_data
 
@@ -31,12 +31,90 @@ def save_as_csv(data, name):
     data.to_csv('Sprungdaten_processed/' + name, index=False)
 
 
+def delete_0_point_jumps(data):
+
+    data = data.query('Sprungtyp != "Strecksprung" & Sprungtyp != "Standsprung" & Sprungtyp != "Hocksprung" & Sprungtyp != "Bücksprung" '
+                      '& Sprungtyp != "Grätschwinkel" & Sprungtyp != "Sitzsprung" & Sprungtyp != "00V"')
+
+    return data
+
+
+def correct_space_errors(data):
+
+    data['Sprungtyp'].replace("Baby-Fliffis C", "Baby- Fliffis C", inplace=True)
+    data['Sprungtyp'].replace("Voll ein Voll aus C", "Voll- ein- voll- aus C", inplace=True)
+    data['Sprungtyp'].replace("Voll- ein- Voll- Aus B", "Voll- ein- voll- aus B", inplace=True)
+    data['Sprungtyp'].replace("Voll ein Halb aus B", "Voll- ein- halb- aus B", inplace=True)
+    data['Sprungtyp'].replace("Halb ein Halb aus B", "Halb- ein- halb- aus B", inplace=True)
+    data['Sprungtyp'].replace("Halb ein Halb aus C", "Halb- ein- halb- aus C", inplace=True)
+    data['Sprungtyp'].replace("Voll ein Voll aus C", "Voll- ein- voll- aus C", inplace=True)
+    data['Sprungtyp'].replace("Voll ein Rudi aus B", "Voll- ein- Rudi- aus B", inplace=True)
+    data['Sprungtyp'].replace("Voll ein Voll aus B", "Voll- ein- voll- aus B", inplace=True)
+    data['Sprungtyp'].replace("Voll ein Voll aus A", "Voll- ein- voll- aus A", inplace=True)
+    data['Sprungtyp'].replace("Voll ein Halb aus C", "Voll- ein- halb- aus C", inplace=True)
+    data['Sprungtyp'].replace("Halb ein Rudy aus B", "Halb- ein- Rudy- aus B", inplace=True)
+    data['Sprungtyp'].replace("Voll- ein- Voll- aus B", "Voll- ein- voll- aus B", inplace=True)
+    data['Sprungtyp'].replace("Voll- ein- Voll- aus A", "Voll- ein- voll- aus A", inplace=True)
+    data['Sprungtyp'].replace("Voll- ein- Voll- aus C", "Voll- ein- voll- aus C", inplace=True)
+    data['Sprungtyp'].replace("Voll- ein - Doppel Aus A", "Voll- ein- doppel- aus A", inplace=True)
+    data['Sprungtyp'].replace("Baby Fliffis C", "Baby- Fliffis C", inplace=True)
+    data['Sprungtyp'].replace("Fliffis Rudi B", "Fliffis- Rudi B", inplace=True)
+    data['Sprungtyp'].replace("51°", "Baby- Fliffis C", inplace=True)
+    data['Sprungtyp'].replace("3/4 Salto Rw A", "3/4 Salto rw A", inplace=True)
+    data['Sprungtyp'].replace("3/4 Salto vW A", "3/4 Salto vw A", inplace=True)
+    data['Sprungtyp'].replace("3/4 Salto Vw A", "3/4 Salto vw A", inplace=True)
+    data['Sprungtyp'].replace("30/R", "3/4 Salto vw A", inplace=True)
+    data['Sprungtyp'].replace("1 3/4 Salto Vw B", "1 3/4 Salto vw B", inplace=True)
+    data['Sprungtyp'].replace("1 3/4 Salto Vw C", "1 3/4 Salto vw C", inplace=True)
+    data['Sprungtyp'].replace("Voll ein 1 3/4 Salto Vw C", "Voll- ein 1 3/4 Salto vw C", inplace=True)
+    data['Sprungtyp'].replace("801°", "Fliffis aus C", inplace=True)
+    data['Sprungtyp'].replace("801<", "Fliffis aus B", inplace=True)
+
+    return data
+
+
+def only_marked_jumps(data):
+
+    data = data.query('Sprungtyp == "Fliffis aus C" or Sprungtyp == "Fliffis aus B" or Sprungtyp == "Fliffis- Rudi C" or Sprungtyp == "Fliffis- Rudi B"'
+                      'or Sprungtyp == "Voll- ein- halb- aus C" or Sprungtyp == "Voll- ein- halb- aus B" or Sprungtyp == "Voll- ein- Rudi- aus A"'
+                      'or Sprungtyp == "Triffis C" or Sprungtyp == "Triffis B" or Sprungtyp == "Triffis- Rudi B" or Sprungtyp == "Schraubensalto"'
+                      'or Sprungtyp == "Doppelsschraubensalto" or Sprungtyp == "Doppelsalto C" or Sprungtyp == "Doppelsalto B" or Sprungtyp == "Doppelsalto A"'
+                      'or Sprungtyp == "1/2 ein 1/2 aus C" or Sprungtyp == "1/2 ein 1/2 aus B" or Sprungtyp == "Halb- ein- Rudy- aus B" or Sprungtyp == "Halb- ein- Rudy- aus C"'
+                      'or Sprungtyp == "Voll- ein- voll- aus C" or Sprungtyp == "Voll- ein- voll- aus A" or Sprungtyp == "Voll- ein- doppel- aus A"'
+                      'or Sprungtyp == "1/2 ein 1/2 aus Triffis C" or Sprungtyp == "1/2 ein 1/2 aus Triffis B"')
+
+    print(data['Sprungtyp'].unique())
+    return data
+
+
 def main():
 
-    all_data = read_all_data()
+    """
+    all_data = read_all_data("all_data.csv")
     data_only_jumps = sort_out_errors(all_data)
 
-    save_as_csv(data_only_jumps, 'data_only_jumps.csv')
+    count_jumptypes = data_only_jumps['Sprungtyp'].value_counts()
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(count_jumptypes)
+
+    # save_as_csv(data_only_jumps, 'data_only_jumps.csv')
+    """
+
+    """
+    data_only_jumps = read_data("data_only_jumps.csv")
+    data_point_jumps = delete_0_point_jumps(data_only_jumps)
+    save_as_csv(data_point_jumps, "data_point_jumps.csv")
+    """
+
+    """
+    data_point_jumps = read_data("data_point_jumps.csv")
+    data_point_jumps = correct_space_errors(data_point_jumps)
+    print(data_point_jumps['Sprungtyp'].unique())
+    save_as_csv(data_point_jumps, "data_point_jumps.csv")
+    """
+
+    data_point_jumps = read_data("data_point_jumps.csv")
+    marked_jumps = only_marked_jumps(data_point_jumps)
 
     return
 
