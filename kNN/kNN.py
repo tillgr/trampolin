260d57ac0c32fda4824a51f88c5c644c52cbbd49
Till Große <till.grosse@web.de>
2021-05-10 22:26:07 +0200

    ADD print loop and metrics

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -5,36 +5,43 @@ from sklearn import neighbors
 from sklearn import preprocessing
 
 if __name__ == '__main__':
-    #read data
-    data = pd.read_csv('../Sprungdaten_processed/averaged_data.csv')
+    # read data
+    train = pd.read_csv('../Sprungdaten_processed/averaged_data/averaged_data_train.csv')
+    test = pd.read_csv('../Sprungdaten_processed/averaged_data/averaged_data_test.csv')
 
-    #get targets
-    # creating labelEncoder
-    le = preprocessing.LabelEncoder()
-    # Converting string labels into numbers.
-    targets = le.fit_transform(data['Sprungtyp'])
-
-    # Split dataset into training set and test set
+    # define data columns
     start_column: str = 'ACC_N'
     end_column: str = 'DJump_Abs_I_z LapEnd'
-    X_train, X_test, y_train, y_test = train_test_split(
-        data.loc[:, start_column:end_column].to_numpy(),
-        targets,
-        test_size=0.3)
 
-    #gleiche anzahl an allen sprüngen
+    # get X_train
+    X_train = train.loc[:, start_column:end_column].to_numpy()
+    # get X_test
+    X_test = test.loc[:, start_column:end_column].to_numpy()
 
-    #neighbours
+    # create labelEncoder
+    le = preprocessing.LabelEncoder()
+    # Convert string labels into numbers
+
+    # get y_train
+    y_train = le.fit_transform(train['Sprungtyp'])
+    # get y_test
+    y_test = le.fit_transform(test['Sprungtyp'])
+
+    # gleiche anzahl an allen sprüngen?
+
+    # neighbours
     n_neighbors = 15
 
     for weights in ['uniform', 'distance']:
-        # we create an instance of Neighbours Classifier and fit the data.
-        clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+        for dist_metrics in ['manhattan', 'chebyshev', 'euclidean']:
+            # we create an instance of Neighbours Classifier and fit the data.
+            clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights, metric=dist_metrics)
 
-        # Train the model using the training sets
-        clf.fit(X_train, y_train)
+            # Train the model using the training sets
+            clf.fit(X_train, y_train)
 
-        # Predict the response for test dataset
-        y_pred = clf.predict(X_test)
+            # Predict the response for test dataset
+            y_pred = clf.predict(X_test)
 
-        print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+            # compare test and predicted targets
+            print(f"Accuracy | {weights} | {dist_metrics}:", metrics.accuracy_score(y_test, y_pred))
