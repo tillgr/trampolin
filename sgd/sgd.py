97412be8ac46ca871489ab5f11e016b6359020aa
AnnaLopatkina <lopaat1009@gmail.com>
2021-05-18 13:09:17 +0200

    add new method for classification sgd

--- sgd/sgd.py
+++ sgd/sgd.py
@@ -4,13 +4,16 @@ import pandas as pd
 from sklearn.linear_model import SGDClassifier
 from sklearn import metrics
 
-if __name__ == '__main__':
-    train_data = pd.read_csv('../Sprungdaten_processed/avg_std_data/avg_std_data_train.csv')
-    test_data = pd.read_csv('../Sprungdaten_processed/avg_std_data/avg_std_data_test.csv')
+def test_1():
+    train_data = pd.read_csv('../Sprungdaten_processed/jumps_time_splits/jumps_time_splits_train_51.csv')
+    test_data = pd.read_csv('../Sprungdaten_processed/jumps_time_splits/jumps_time_splits_test_51.csv')
 
     # get_features (X)
-    start_column: str = 'avg_ACC_N'
-    end_column: str = 'DJump_Abs_I_z LapEnd'
+    start_column: str = 'Acc_x_Fil_1'
+    end_column: str = 'Gyro_z_Fil_50'
+
+    '''start_column: str = 'ACC_N'
+    end_column: str = 'Gyro_z_Fil' '''
 
     X_train = train_data.loc[:, start_column:end_column].to_numpy()
     # get target (y)
@@ -19,29 +22,51 @@ if __name__ == '__main__':
     X_test = test_data.loc[:, start_column:end_column].to_numpy()
     y_test = np.array((test_data['Sprungtyp']))
 
-    # shuffle train
-    idx = np.arange(X_train.shape[0])
-    np.random.seed(25)
-    np.random.shuffle(idx)
-    X_train_ = X_train[idx]
-    y_train_ = y_train[idx]
+def test2():
 
-    # shuffle test
-    idx = np.arange(X_test.shape[0])
-    np.random.seed(25)
-    np.random.shuffle(idx)
-    X_test_ = X_test[idx]
-    y_test_ = y_test[idx]
-    for losses in ['log', 'modified_huber', 'squared_hinge', 'perceptron']:  #Accuracy: log , l1 , 10000:  0.7558823529411764
+    train_data = pd.read_csv('../Sprungdaten_processed/jumps_time_splits/jumps_time_splits_train_21.csv')
+    test_data = pd.read_csv('../Sprungdaten_processed/jumps_time_splits/jumps_time_splits_test_21.csv')
+
+    # get_features (X)
+    start_column: str = 'Acc_x_Fil_1'
+    end_column: str = 'DJump_Abs_I_z LapEnd'
+
+    X_train = train_data.loc[:, start_column:end_column].to_numpy()
+    y_train = np.array((train_data['Sprungtyp']))
+
+    X_test = test_data.loc[:, start_column:end_column].to_numpy()
+    y_test = np.array((test_data['Sprungtyp']))
+
+
+if __name__ == '__main__':
+
+    train = pd.read_csv("../Sprungdaten_processed/jumps_time_splits/jumps_time_splits_train_101.csv")
+    test = pd.read_csv("../Sprungdaten_processed/jumps_time_splits/jumps_time_splits_test_101.csv")
+    train_avg = pd.read_csv("../Sprungdaten_processed/averaged_data/averaged_data_train.csv")
+    test_avg = pd.read_csv("../Sprungdaten_processed/averaged_data/averaged_data_test.csv")
+
+    train_merged = train.merge(train_avg)
+    test_merged = test.merge(test_avg)
+
+    start_column: str = 'Acc_x_Fil_1'
+    end_column: str = 'DJump_Abs_I_z LapEnd'
+
+    X_train = train_merged.loc[:, start_column:end_column].to_numpy()
+    y_train = np.array((train_merged['Sprungtyp']))
+
+    X_test = test_merged.loc[:, start_column:end_column].to_numpy()
+    y_test = np.array((test_merged['Sprungtyp']))
+
+    for losses in ['log', 'modified_huber', 'squared_hinge', 'perceptron']:
         for penalty in ['l2', 'l1', 'elasticnet']:
-            for maxi in [1000, 10000, 100000, 1000000, 10000000]:
+            for maxi in [10000]:
                 clf = SGDClassifier(loss=losses, penalty=penalty, alpha=0.0001,
                                     l1_ratio=0.15, fit_intercept=True, max_iter=maxi,
                                     tol=0.001, shuffle=True, verbose=0, epsilon=0.1,
-                                    n_jobs=None, random_state=None, learning_rate='optimal',
+                                    n_jobs=None, random_state=10, learning_rate='optimal',
                                     eta0=0.0, power_t=0.5, early_stopping=False, validation_fraction=0.1,
                                     n_iter_no_change=5, class_weight=None,
-                                    warm_start=False, average=False).fit(X_train_, y_train_)
-                y_pred = clf.predict(X_test_)
-                if metrics.accuracy_score(y_test_, y_pred) > 0.74:
-                    print(f"Accuracy: {losses} , {penalty} , {maxi}: ", metrics.accuracy_score(y_test_, y_pred))
+                                    warm_start=False, average=False).fit(X_train, y_train)
+                y_pred = clf.predict(X_test)
+                if metrics.accuracy_score(y_test, y_pred) > 0.87:
+                    print(f"Accuracy: {losses} , {penalty} , {maxi}: ", metrics.accuracy_score(y_test, y_pred))
