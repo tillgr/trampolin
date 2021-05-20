e648859f0bac49ee0f6c3324f59fa5b40dc90b29
Lukas <Lukas.Petzold1@mailbox.tu-dresden.de>
2021-05-20 18:07:01 +0200

    Fixed metrics

--- random_classifier.py
+++ random_classifier.py
@@ -4,13 +4,14 @@ import os
 import openpyxl
 import random
 from sklearn.metrics import accuracy_score, confusion_matrix
+import sklearn as sk
 
 
 def main():
     name = "data_point_jumps"
     trainings_data = pd.read_csv("Sprungdaten_processed/" + name + "/" + name + "_train.csv")
     test_data = pd.read_csv('Sprungdaten_processed/' + name + '/' + name + '_test.csv')
-    classify(trainings_data, test_data, 10000)
+    classify(trainings_data, test_data, 100)
     return
 
 
@@ -29,42 +30,32 @@ def classify(trainings_data, test_data, loops=None):
         c = random.sample(r, len(test_data['SprungID'].unique()))
 
         acc = accuracy_score(label, c)
-        tp, fp, tn, fn = metrics(label, c)
-        print(tp, fp, fn, tn)
-        # Precision
-        prec = tp/(tp+fp)
-        # Recall
-        rec = tp/(fn+tp)
-        # F1 Score
-        f = 2*(prec*rec) / (prec + rec)
-        # Youden
-        youden = rec + (tn/(tn + fp)) - 1
+        prec, rec, f, youden = metrics(label, c)
 
     else:
         acc = []
-        prec = []
-        rec = []
-        youden = []
-        f = []
+        prec_list = []
+        rec_list = []
+        youden_list = []
+        f_list = []
         best = 0
         for i in range(loops):
             print(str(i) + '/' + str(loops))
             c = random.sample(r, len(test_data['SprungID'].unique()))
             a = accuracy_score(label, c)
             acc.append(a)
-            tp, fp, tn, fn = metrics(label, c)
-            prec.append((tp/(tp+fp)))
-            rec.append((tp/(fn+tp)))
-            f.append((2*(prec[-1] * rec[-1]) / (prec[-1] + rec[-1])))
-            y = (rec[-1] + (tn/(tn + fp)) - 1)
-            youden.append(y)
-            if y > best:
-                best = y
+            prec, rec, f, youden = metrics(label, c)
+            prec_list.append(prec)
+            rec_list.append(rec)
+            f_list.append(f)
+            youden_list.append(youden)
+            if youden > best:
+                best = youden
         acc = sum(acc) / len(acc)
-        prec = sum(prec) / len(prec)
-        rec = sum(rec) / len(rec)
-        f = sum(f) / len(f)
-        youden = sum(youden) / len(youden)
+        prec = sum(prec_list) / len(prec_list)
+        rec = sum(rec_list) / len(rec_list)
+        f = sum(f_list) / len(f_list)
+        youden = sum(youden_list) / len(youden_list)
         print('best Youden:' + str(best))
     print("Accuracy: " + str(acc))
     print("Youden: " + str(youden))
@@ -75,25 +66,44 @@ def classify(trainings_data, test_data, loops=None):
 
 
 def metrics(label, c):
-    conf = confusion_matrix(label, c)
-    tp = sum(conf.diagonal())
-    fp = 0
-    fn = 0
-    tn = 0
-    for r in range(len(conf)):
-        # fp
-        row = conf[r]
-        row = np.delete(row, r)
-        fp += sum(row)
-        # fn
-        col = conf[:, r]
-        col = np.delete(col, r)
-        fn += sum(col)
-        # tn
-        sub_array = np.delete(conf, r, 0)
-        sub_array = np.delete(sub_array, r, 1)
-        tn += sum(sum(sub_array))
-    return tp, fp, tn, fn
+
+    mcm = sk.metrics.multilabel_confusion_matrix(label, c)
+    tn = mcm[:, 0, 0]
+    fn = mcm[:, 1, 0]
+    tp = mcm[:, 1, 1]
+    fp = mcm[:, 0, 1]
+
+    mean_prec = 0.0
+    mean_rec = 0.0
+    mean_f = 0.0
+    mean_youden = 0.0
+
+    num_classes = len(tp)
+
+    for i in range(num_classes):
+        prec = 0.0
+        rec = 0.0
+
+        # check for division by 0
+        if (tp[i] + fp[i]) != 0:
+            prec = tp[i] / (tp[i] + fp[i])
+            mean_prec += prec
+        if (fn[i] + tp[i]) != 0:
+            rec = tp[i] / (fn[i] + tp[i])
+            mean_rec += rec
+        if (prec + rec) != 0:
+            mean_f += 2 * (prec * rec) / (prec + rec)
+        if (tn[i] + fp[i]) != 0:
+            mean_youden += rec + (tn[i] / (tn[i] + fp[i])) - 1
+
+    mean_prec /= num_classes
+    mean_rec /= num_classes
+    mean_f /= num_classes
+    mean_youden /= num_classes
+
+    # print("prec = %f, rec = %f, f-score = %f, youden = %f"%(mean_prec, mean_rec, mean_f, mean_youden))
+
+    return mean_prec, mean_rec, mean_f, mean_youden
 
 
 def get_target(test_data):
