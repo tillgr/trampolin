899973a2fadb515e4a04825cddf7c4e0a46f1601
Till Große <till.grosse@web.de>
2021-05-31 20:50:40 +0200

    ADD kNN run all sets with metrics

--- GNB/GB.py
+++ GNB/GB.py
@@ -35,8 +35,8 @@ if __name__ == '__main__':
     X_test = test.loc[:, start_column:end_column].to_numpy()
     '''
 
-    train_merged = train.merge(train_avg)
-    test_merged = test.merge(test_avg)
+    #train_merged = train.merge(train_avg)
+    #test_merged = test.merge(test_avg)
 
     # percentage 10
     train_merged = pd.read_csv("../Sprungdaten_processed/percentage/10/vector_percentage_10_train.csv")
@@ -65,21 +65,29 @@ if __name__ == '__main__':
 
 
     # we create an instance of Neighbours Classifier and fit the data.
-    clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,
-    max_depth=1, random_state=0)
+    for estimator in [100]:
+        for depth in [1, 2]:
+            for learning_rate in [0.1, 0.01, 0.001, 0.0001]:
+                clf = GradientBoostingClassifier(n_estimators=estimator, learning_rate=learning_rate,
+                max_depth=depth, random_state=2)
 
-    for i in range(0, 4):
-        # Train the model using the training sets
-        clf.fit(X_train, y_train)
+                print('///////////////////////////////////////////////////////////////')
+                print(f"estimators: {estimator}")
+                print(f"learning_rate: {learning_rate}")
+                print(f"depth: {depth}")
 
-    # Predict the response for test dataset
-    y_pred = clf.predict(X_test)
+                # for i in range(0, 4):
+                    # Train the model using the training sets
+                clf.fit(X_train, y_train)
 
-    # compare test and predicted targets
-    #print(f"PARAMETER:  weights: {weights} | metric: {dist_metrics}")
-    print(f"Accuracy self: ", metrics.accuracy_score(y_test, y_pred))
-    print(f"Accuracy f1 score weighted: {f1_score(y_test, y_pred, average='weighted')} ")
-    mean_prec, mean_rec, mean_f, mean_youden = rc_metrics(y_test, y_pred)
-    print(f"Accuracy f1 score: {str(mean_f.round(5))}")
-    print(f"Accuracy youden score: {str(mean_youden.round(5))}")
-    print("--------------------------------------------------------------")
+                # Predict the response for test dataset
+                y_pred = clf.predict(X_test)
+
+                # compare test and predicted targets
+                #print(f"PARAMETER:  weights: {weights} | metric: {dist_metrics}")
+                print(f"Accuracy self: ", metrics.accuracy_score(y_test, y_pred))
+                print(f"Accuracy f1 score weighted: {f1_score(y_test, y_pred, average='weighted')} ")
+                mean_prec, mean_rec, mean_f, mean_youden = rc_metrics(y_test, y_pred)
+                print(f"Accuracy f1 score: {str(mean_f.round(5))}")
+                print(f"Accuracy youden score: {str(mean_youden.round(5))}")
+                print("--------------------------------------------------------------")
