0ccf5763b707c53407865538e54763807bfb4088
Kygri <lisa.riedel@mailbox.tu-dresden.de>
2021-06-08 18:14:05 +0200

    fixing lower case problen in xlsx column names

--- all_data.py
+++ all_data.py
@@ -41,8 +41,10 @@ def convert_comma_to_dot(x):
 def main():
     first_round = True
     first_temp = True
+    global_xlsx_col_names = []
 
     folders = os.listdir('Sprungdaten Innotramp')
+    folders = ['2019.09.30']
     for folder in folders:
         files = os.listdir("Sprungdaten Innotramp/" + folder)
         for i in range(len(files[:int(len(files) / 2)])):           # we can read the corresponding sprungzuordnung and rohdaten this way
@@ -50,10 +52,10 @@ def main():
             csv_data = read_csv_data("Sprungdaten Innotramp/" + folder + "/" + files[i])
             xlsx_data = read_xlsx_data("Sprungdaten Innotramp/" + folder + "/" + files[i + int(len(files) / 2)])
 
-            csv_data['Time'] = np.round(csv_data['Time'].apply(convert_comma_to_dot), 3)
+            csv_data['Time'] = np.round(csv_data['Time'].apply(convert_comma_to_dot), 3)    # convert time stucture
             xlsx_data["Zeit"] = xlsx_data["Zeit"].apply(convert_to_numeric)
 
-            temp_data = pd.DataFrame(columns=csv_data.drop(['Dist'], axis=1).columns)
+            temp_data = pd.DataFrame(columns=csv_data.drop(['Dist'], axis=1).columns)   # df for csv data
 
             temp_data = temp_data.append(csv_data.drop(['Dist'], axis=1))
 
@@ -66,19 +68,21 @@ def main():
 
                 end_time = row[1]['cumultime']
                 join_times = np.arange(start_time, end_time, 0.002)                 # creates all times with 0.002 steps from start to end
-                SprungID = row[1]['Messung'] + "-" + str(row[1]['Lap#'])            # create an unique ID for each jump
+                sprungID = row[1]['Messung'] + "-" + str(row[1]['Lap#'])            # create an unique ID for each jump
 
 
-                multiply_array = np.array([SprungID, row[1]['Sprungtyp']])
+                multiply_array = np.array([sprungID, row[1]['Sprungtyp']])
                 for col in xlsx_data.columns:
                     if 'DJump' in col:
                         multiply_array = np.append(multiply_array, row[1][col])
                 multiply_array = np.transpose(np.repeat(multiply_array.reshape(len(multiply_array), 1), len(join_times), axis=1))         # this array will be multiplied when joined with the rohdaten
-                names = ['SprungID', 'Sprungtyp'] + [col for col in xlsx_data.columns if 'DJump' in col]
-                temp_sprungzuordnung = pd.DataFrame(multiply_array, columns=names)
+                if first_temp:                                                      # for fixing lower case problem in xlsx column names
+                    global_xlsx_col_names = ['SprungID', 'Sprungtyp'] + [col for col in xlsx_data.columns if 'DJump' in col]
+                temp_sprungzuordnung = pd.DataFrame(multiply_array, columns=global_xlsx_col_names)
                 temp_sprungzuordnung['Time'] = np.round(join_times, 3)
                 if first_temp:
                     sprungzuordnung = pd.DataFrame(columns=temp_sprungzuordnung.columns)
+
                     first_temp = False
                 sprungzuordnung = sprungzuordnung.append(temp_sprungzuordnung)
 
@@ -101,7 +105,6 @@ def main():
 
     return
 
-    # TODO: 30.09.2019, 01.10.2019, 07.10.2019
 
 
 if __name__ == '__main__':
