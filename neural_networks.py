fe20fd18a75fa71729e773a6bcc5509feb75ba35
Lukas <Lukas.Petzold1@mailbox.tu-dresden.de>
2021-06-10 14:48:02 +0200

    tested sklearn gridsearch

--- neural_networks.py
+++ neural_networks.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import keras
-from keras.models import Model
+from keras.models import Model, Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Input, Flatten, Dropout, SpatialDropout2D, AveragePooling2D
 from keras import backend as k
 import matplotlib.pyplot as plt
@@ -9,26 +9,26 @@ from matplotlib.colors import ListedColormap
 import tensorflow as tf
 import shap
 import sklearn
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from keras.wrappers.scikit_learn import KerasClassifier
 
 
 def prepare_data():
 
-    # data_train = pd.read_csv("Sprungdaten_processed/same_length/same_length_padding_0_train.csv")
-    # data_test = pd.read_csv("Sprungdaten_processed/same_length/same_length_padding_0_test.csv")
-    data_train = pd.read_csv("Sprungdaten_processed/percentage/2/percentage_mean_2_train.csv")
-    data_test = pd.read_csv("Sprungdaten_processed/percentage/2/percentage_mean_2_test.csv")
+    data_train = pd.read_csv("Sprungdaten_processed/without_preprocessed/percentage/1/percentage_mean_1_train.csv")
+    data_test = pd.read_csv("Sprungdaten_processed/without_preprocessed/percentage/1/percentage_mean_1_test.csv")
 
     # data_train = pd.read_csv("Sprungdaten_processed/percentage/5/val/percentage_mean_5_80_10_10_train.csv")
     # data_test = pd.read_csv("Sprungdaten_processed/percentage/5/val/percentage_mean_5_80_10_10_test.csv")
     # data_val = pd.read_csv("Sprungdaten_processed/percentage/5/val/percentage_mean_5_80_10_10_val.csv")
 
-    data_train = data_train.drop(['DJump_SIG_I_x LapEnd', 'DJump_SIG_I_y LapEnd', 'DJump_SIG_I_z LapEnd',
-                                  'DJump_Abs_I_x LapEnd', 'DJump_Abs_I_y LapEnd', 'DJump_Abs_I_z LapEnd'], axis=1)
-    data_test = data_test.drop(['DJump_SIG_I_x LapEnd', 'DJump_SIG_I_y LapEnd', 'DJump_SIG_I_z LapEnd',
-                                  'DJump_Abs_I_x LapEnd', 'DJump_Abs_I_y LapEnd', 'DJump_Abs_I_z LapEnd'], axis=1)
-
-    data_train = data_train.drop(['ACC_N_ROT_filtered'], axis=1)
-    data_test = data_test.drop(['ACC_N_ROT_filtered'], axis=1)
+    # DJump_SIG_I_S , DJump_ABS_I_S , DJump_I_ABS_S
+    #data_train = data_train.drop([col for col in data_train.columns if 'DJump_SIG_I_S' in col], axis=1)
+    #data_train = data_train.drop([col for col in data_train.columns if 'DJump_ABS_I_S' in col], axis=1)
+    #data_train = data_train.drop([col for col in data_train.columns if 'DJump_I_ABS_S' in col], axis=1)
+    #data_test = data_test.drop([col for col in data_test.columns if 'DJump_SIG_I_S' in col], axis=1)
+    #data_test = data_test.drop([col for col in data_test.columns if 'DJump_ABS_I_S' in col], axis=1)
+    #data_test = data_test.drop([col for col in data_test.columns if 'DJump_I_ABS_S' in col], axis=1)
 
     x_train = []
     y_train = []
@@ -83,17 +83,13 @@ def prepare_data():
 
 def prepare_data_oneliner():
 
-    data_train = pd.read_csv("Sprungdaten_processed/percentage/25/vector_percentage_25_train.csv")
-    data_test = pd.read_csv("Sprungdaten_processed/percentage/25/vector_percentage_25_test.csv")
+    data_train = pd.read_csv("Sprungdaten_processed/with_preprocessed/percentage/25/vector_percentage_25_train.csv")
+    data_test = pd.read_csv("Sprungdaten_processed/with_preprocessed/percentage/25/vector_percentage_25_test.csv")
 
     x_train = data_train.drop('Sprungtyp', axis=1)
-    x_train = x_train.drop(['SprungID', 'DJump_SIG_I_x LapEnd', 'DJump_SIG_I_y LapEnd', 'DJump_SIG_I_z LapEnd',
-                            'DJump_Abs_I_x LapEnd', 'DJump_Abs_I_y LapEnd', 'DJump_Abs_I_z LapEnd'], axis=1)
+    x_train = x_train.drop(['SprungID'], axis=1)
     x_test = data_test.drop('Sprungtyp', axis=1)
-    x_test = x_test.drop(['SprungID', 'DJump_SIG_I_x LapEnd', 'DJump_SIG_I_y LapEnd', 'DJump_SIG_I_z LapEnd',
-                          'DJump_Abs_I_x LapEnd', 'DJump_Abs_I_y LapEnd', 'DJump_Abs_I_z LapEnd'], axis=1)
-    x_train = x_train.drop([col for col in x_train.columns if 'ACC_N_ROT_filtered' in col], axis=1)
-    x_test = x_test.drop([col for col in x_test.columns if 'ACC_N_ROT_filtered' in col], axis=1)
+    x_test = x_test.drop(['SprungID'], axis=1)
 
     num_columns = len(x_train.columns)
 
@@ -106,26 +102,6 @@ def prepare_data_oneliner():
     return x_train, y_train, x_test, y_test, num_columns
 
 
-def build_model_testing(jump_data_length, num_columns):
-
-    first_input = Input(shape=(jump_data_length, num_columns, 1), name="first_input")
-    x = Conv2D(221, kernel_size=(5, 5), padding="same", activation='tanh')(first_input)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(237, kernel_size=(3, 3), padding="same", activation='tanh')(x)
-    x = Conv2D(207, kernel_size=(4, 4), padding="same", activation='tanh')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    x = Flatten()(x)
-    x = Dense(1438, activation='tanh')(x)
-    x = Dense(1557, activation='tanh')(x)
-    x = Dense(41, activation='softmax', name="output")(x)
-
-    model = Model(inputs=first_input, outputs=x)
-    model.compile(loss='categorical_crossentropy', optimizer='Nadam', metrics=['accuracy'])
-
-    return model
-
-
 def build_model(jump_data_length, num_columns, c, kernel, pool, d, act_func, loss, optim):
 
     first_input = Input(shape=(jump_data_length, num_columns, 1), name="first_input")
@@ -136,7 +112,7 @@ def build_model(jump_data_length, num_columns, c, kernel, pool, d, act_func, los
     x = Flatten()(x)
     for i in range(d):
         x = Dense(32 * d, activation=act_func)(x)
-    x = Dense(41, activation='softmax', name="output")(x)
+    x = Dense(45, activation='softmax', name="output")(x)
 
     model = Model(inputs=first_input, outputs=x)
     model.compile(loss=loss, optimizer=optim, metrics=['accuracy', keras.metrics.TruePositives(), keras.metrics.TrueNegatives(), keras.metrics.FalsePositives(), keras.metrics.FalseNegatives()])
@@ -144,35 +120,40 @@ def build_model(jump_data_length, num_columns, c, kernel, pool, d, act_func, los
     return model
 
 
-def grid_search_build(x_train, y_train, x_test, y_test, jump_data_length):
+def build_model_grid(jump_data_length=20, num_columns=16, c=1, act_func='tanh', loss='categorical_crossentropy', optim='Nadam'):
 
-    best_score = 0
-    best_model = 0
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), padding="same", activation=act_func, input_shape=(jump_data_length, num_columns, 1)))
+    for i in range(c):
+        model.add(Conv2D(32 * (i + 1), kernel_size=(3, 3), padding="same", activation=act_func))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding="same"))
+    model.add(Flatten())
+    model.add(Dense(64, activation=act_func))
+    model.add(Dense(45, activation='softmax', name="output"))
 
-    for c in range(4):
-        for kernel in range(2, 5):
-            for pool in range(2, 5):
-                for d in range(2, 5):
-                    for act_func in ['tanh', 'elu']:
-                        for loss in ['categorical_crossentropy', 'poisson', 'kl_divergence']:
-                            for optim in ['adam', 'Adamax', 'Nadam']:
-                                model_parameters = [c, kernel, pool, d, act_func, loss, optim]
-                                print("Training model with: " + str(model_parameters))
+    """first_input = Input(shape=(jump_data_length, num_columns, 1), name="first_input")
+    x = Conv2D(32, kernel_size=(3, 3), padding="same", activation=act_func)(first_input)
+    x = Conv2D(64, kernel_size=(3, 3), padding="same", activation=act_func)(x)
+    x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
+    #x = Dropout(dropout_rate)
+    x = Flatten()(x)
+    x = Dense(64, activation=act_func)(x)
+    x = Dense(45, activation='softmax', name="output")(x)
 
-                                model = build_model(jump_data_length, c, kernel, pool, d, act_func, loss, optim)
-                                model.fit(x_train, y_train, batch_size=32, epochs=12, verbose=1)
-                                score = model.evaluate(x_test, y_test, verbose=1)
-                                if score[1] > best_score:
-                                    best_score = score[1]
-                                    best_model = model
-                                    best_model_parameters = model_parameters
-                                    print("Best model with: " + str(best_model_parameters))
+    model = Model(inputs=first_input, outputs=x)"""
+    model.compile(loss=loss, optimizer=optim, metrics=['accuracy', keras.metrics.TruePositives(), keras.metrics.TrueNegatives(), keras.metrics.FalsePositives(), keras.metrics.FalseNegatives()])
 
-    print("The best model is: c = %d, kernel = %d, pool = %d, d = %d, act_func = %s, loss = %s, optim = %s"%(
-        best_model_parameters[0], best_model_parameters[1], best_model_parameters[2], best_model_parameters[3],
-        best_model_parameters[4], best_model_parameters[5], best_model_parameters[6]))
+    return model
 
-    return best_model
+
+def grid_search_build(x_train, x_test, y_train, y_test, model, param_grid, cv=10, scoring_fit='neg_mean_squared_error'):
+
+    gs = GridSearchCV(estimator=model, param_grid=param_grid, cv=cv, n_jobs=-1, scoring=scoring_fit, verbose=2)
+    fitted_model = gs.fit(x_train, y_train)
+
+    pred = fitted_model.predict(x_test)
+
+    return fitted_model, pred
 
 
 def run_multiple_times(jump_data_length, num_columns, runs, conv, kernel, pool, dense, act_func, loss, optim, epochs, x_train, y_train, x_test, y_test):
@@ -181,10 +162,10 @@ def run_multiple_times(jump_data_length, num_columns, runs, conv, kernel, pool, 
     mean_score = 0
 
     for i in range(runs):
-        callback = keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+        #callback = keras.callbacks.EarlyStopping(monitor='loss', patience=6)
         model = build_model(jump_data_length, num_columns, conv, kernel, pool, dense, act_func, loss, optim)
         #model = build_model_testing(jump_data_length, num_columns)
-        model.fit(x_train, y_train, batch_size=32, epochs=epochs, verbose=1, callbacks=[callback])
+        model.fit(x_train, y_train, batch_size=32, epochs=epochs, verbose=1)
         score = model.evaluate(x_test, y_test, verbose=1)
         mean_score += score[1]
         if score[1] > best_score:
@@ -235,9 +216,9 @@ def run_multiple_times_oneliner(num_columns, runs, act_func, loss, optim, epochs
     mean_score = 0
 
     for i in range(runs):
-        callback = keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+        #callback = keras.callbacks.EarlyStopping(monitor='loss', patience=6)
         model = build_model_oneliner(num_columns, act_func, loss, optim)
-        model.fit(x_train, y_train, batch_size=32, epochs=epochs, verbose=1, callbacks=[callback])
+        model.fit(x_train, y_train, batch_size=32, epochs=epochs, verbose=1)
         score = model.evaluate(x_test, y_test, verbose=1)
         mean_score += score[1]
         if score[1] > best_score:
@@ -266,21 +247,35 @@ def run_multiple_times_oneliner(num_columns, runs, act_func, loss, optim, epochs
 
 def main():
 
-    #x_train, y_train, x_test, y_test, jump_data_length, num_columns = prepare_data()
-    x_train, y_train, x_test, y_test, num_columns = prepare_data_oneliner()
+    x_train, y_train, x_test, y_test, jump_data_length, num_columns = prepare_data()
+    # x_train, y_train, x_test, y_test, num_columns = prepare_data_oneliner()
 
     # model = grid_search_build(x_train, y_train, x_test, y_test, jump_data_length)
-    # model = build_model_testing(jump_data_length, x_train, y_train)
     # model = run_multiple_times(10, jump_data_length, 3, 3, 2, 2, 'tanh', 'kl_divergence', 'Nadam', x_train, y_train, x_test, y_test, 20)
-    #model = run_multiple_times(jump_data_length, num_columns, runs=10, conv=1, kernel=3, pool=2, dense=2, act_func='tanh', loss='categorical_crossentropy', optim='Nadam', epochs=40, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
-    #model = run_multiple_times(jump_data_length, num_columns, runs=10, conv=3, kernel=3, pool=2, dense=2, act_func='tanh', loss='kl_divergence', optim='Nadam', epochs=30, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+    #model = run_multiple_times(jump_data_length, num_columns, runs=1, conv=1, kernel=3, pool=2, dense=2, act_func='tanh', loss='categorical_crossentropy', optim='Nadam', epochs=40, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+    #model = run_multiple_times(jump_data_length, num_columns, runs=1, conv=3, kernel=3, pool=2, dense=2, act_func='tanh', loss='kl_divergence', optim='Nadam', epochs=40, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
 
-    model = run_multiple_times_oneliner(num_columns, runs=10, act_func='tanh', loss='kl_divergence', optim='adam', epochs=60, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+    # model = run_multiple_times_oneliner(num_columns, runs=10, act_func='tanh', loss='kl_divergence', optim='adam', epochs=60, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
 
+    """
+    param_grid = {'jump_data_length': [jump_data_length], 'num_columns': [num_columns], 'epochs': [40],
+                  'batch_size': [32], 'optim': ['adam', 'Nadam'], 'c': [1, 2, 3],
+                  'act_func': ['tanh', 'relu'], 'loss': ['categorical_crossentropy', 'kl_divergence']}
+    model = KerasClassifier(build_fn=build_model_grid, verbose=0)
+    model, pred = grid_search_build(x_train, x_test, y_train, y_test, model, param_grid, cv=2, scoring_fit='neg_log_loss')
+
+    print(model.best_score_)
+    print(model.best_params_)
+    """
+    #"""
+    model = run_multiple_times(jump_data_length, num_columns, runs=5, conv=1, kernel=3, pool=2, dense=2,
+                               act_func='tanh', loss='kl_divergence', optim='Nadam', epochs=40,
+                               x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
     model.evaluate(x_test, y_test, verbose=1)
+    #"""
 
     shap.initjs()
-    #"""
+    """
     # DFF
     background = shap.sample(x_train, 100)
     explainer = shap.KernelExplainer(model, background)
@@ -288,8 +283,7 @@ def main():
 
     shap.summary_plot(shap_values, x_test, plot_type='bar')
     shap.summary_plot(shap_values[0], x_test)
-
-    #"""
+    """
 
 
     # CNN
@@ -307,7 +301,7 @@ def main():
     e = shap.DeepExplainer(model, background)
     shap_values = e.shap_values(x_test[i])
     shap.image_plot(shap_values, -x_test[i])  # , labels=list(y_test.columns))
-
+    
     # Confusion matrix to find mistakes in classification
     cm = sklearn.metrics.confusion_matrix(y_test.idxmax(axis=1), pd.DataFrame(model.predict(x_test), columns=y_test.columns).idxmax(axis=1))
     disp = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=y_test.columns)
