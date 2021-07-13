66f43c61b520f39826717ad782acadff9788dce4
AnnaLopatkina <lopaat1009@gmail.com>
2021-07-13 14:56:36 +0200

    Jump detection

--- sgd/jump_detection.py
+++ sgd/jump_detection.py
@@ -1,11 +1,10 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
 def number_of_jumps(data):
     jump_list = []
-    for i in range(100000):
+    for i in range(10000):
         jump_list.append(data[i])
     jumps = list(dict.fromkeys(jump_list))
     return print(len(jumps))
@@ -13,17 +12,17 @@ def number_of_jumps(data):
 
 def data_size(data):
     jump_list = []
-    for i in range(100000):
+    for i in range(10000):
         jump_list.append(data[i])
     print(len(jump_list))
 
 
 def cost(threshold: int):
     count = 0
-    for i in range(100000):
+    for i in range(10000):
         if y[i] > y[i + 1] and y[i] > y[i - 1] and y[i] > threshold:
             count += 1
-            if count > jumps_amount:
+            if count > 10:
                 break
     return count
 
@@ -41,20 +40,21 @@ def estimate_threshhold(threshold: int):
 
 if __name__ == '__main__':
     # jumps_amount = 5927 / 5463628 # jumps amount = 101 / 100000
-    # jumps_amount = 3 / 2715 # jumps_amount = 1072 / 1000000 # all = 5463628
-    threshold = 0
-    jumps_amount = 101
+    # jumps_amount = 3 / 2715 # jumps_amount = 1072 / 1000000 # all = 5463628 # jumps_amount = 10 / 10000
+    thresholdMin = 0
+    thresholdMax = 100
+    jumps_amount = 10
     data = pd.read_csv("../sgd/all_data_new.csv")
     x = np.array((data['Time']))
     y = np.array((data['Acc_N_Fil']))
     z = np.array((data['SprungID']))
     # plt.plot(x[0:2700], y[0:2700])
     # plt.show()
-
     # number_of_jumps(z);
-    estimate_threshhold(threshold)
+    thresholdMin = estimate_threshhold(thresholdMin)
+    thresholdMax = estimate_threshhold(thresholdMax)
 
-    for i in range(100000):
-        if y[i] > y[i + 1] and y[i] > y[i - 1] and y[i] > threshold:
+    for i in range(10000):
+        if y[i] > y[i + 1] and y[i] > y[i - 1] and y[i] > thresholdMax:
             print(f" index: {i + 2}:, time: {x[i]}, acceleration: {y[i]}, id: {z[i]}")
-    print(threshold)
+    print(f"the threshold is between {thresholdMin} and {thresholdMax}")
